5fc3a1ad44c35ee56c3f3122781bb586c0062c5f
jscielo <jscielo@gmail.com>
2017-01-20 18:02:58 -0500

    20170120

--- prj_001/init.js
+++ prj_001/init.js
@@ -1,11 +1,20 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+/*
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
 
+
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+*/
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.listen(3003, function() {
-  console.log('Node Server On');
+  console.log('Node Server Ready!!!');
 });
